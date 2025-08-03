6ef0360286c1972117d65a556ecf517340c481c0
Igor Vinicius Santos Vicente <igor.v.vicente@aluno.senai.br>
2025-08-03 17:46:16 -0300

    Meu portifólio atualizado

---
+++ script.js
@@ -0,0 +1,9 @@
+
+// Efeito de rolagem suave para os links do menu
+document.querySelectorAll('nav a').forEach(link => {
+    link.addEventListener('click', function(e) {
+        e.preventDefault();
+        const target = document.querySelector(this.getAttribute('href'));
+        target.scrollIntoView({ behavior: 'smooth' });
+    });
+});
